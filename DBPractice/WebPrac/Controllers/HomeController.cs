4b984d52fb13131063c0d8a7058f2c28e9f6f0f5
H.M.Zahid <bcsf15m019@gmail.com>
2018-05-11 23:51:30 +0500

    Assignment7 (Product Management System using AJAX started)

---
+++ DBPractice/WebPrac/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPrac.Security;
+
+namespace WebPrac.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Test()
+        {
+            int c = 0;
+            int b = 0;
+            int a = 0;
+            return null;
+        }
+        public ActionResult Admin()
+        {
+            if (SessionManager.IsValidUser)
+            {
+
+                if (SessionManager.User.IsAdmin)
+                {
+                    return View();
+                }
+                else
+                {
+                    return RedirectToAction("NormalUser");
+                }
+            }
+            else
+            {
+                return Redirect("~/User/Login");
+            }
+        }
+        public ActionResult NormalUser()
+        {
+            if (SessionManager.IsValidUser)
+            {
+
+                if (SessionManager.User.IsAdmin)
+                {
+                    return RedirectToAction("Admin");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            else
+            {
+                return Redirect("~/User/Login");
+            }
+        }
+    }
+}
